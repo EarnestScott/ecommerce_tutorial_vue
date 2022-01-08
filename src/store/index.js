ddef3072c4279cb5ca6abcf7a86ada42d6e981ca
Earnest Scott <earnestscott@Earnests-MBP.lan>
2022-01-08 14:58:17 -0800

    added state mutations to intiitalize store and add items to cart

--- src/store/index.js
+++ src/store/index.js
@@ -10,6 +10,27 @@ export default createStore({
     isLoading: false
   },
   mutations: {
+    initializeStore(state) {
+      // check to see if cart already exists in local storage
+      if (localStorage.getItem('cart')) {
+        state.cart = JSON.parse(localStorage.getItem('cart'))
+      }
+      else {
+        localStorage.setItem('cart', JSON.stringify(state.cart))
+      }
+    },
+    addToCart(state, item) {
+      const exists = state.cart.items.filter(elem => elem.product.id === item.product.id)
+
+      if (exists.length) {
+        exists[0].quantity = parseInt(exists[0].quantity) + parseInt(item.quantity)
+      }
+      else {
+        state.cart.items.push(item)
+      }
+
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    }
   },
   actions: {
   },
