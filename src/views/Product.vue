61523b7566a959a76376c9c4db0de109fede3f8e
Earnest Scott <earnestscott@Earnests-MBP.lan>
2022-01-08 15:20:58 -0800

    success toast for successful add-to-cart

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -27,6 +27,8 @@
 
 <script>
 import axios from "axios";
+import { toast } from "bulma-toast";
+
 export default {
   name: "Product",
   data() {
@@ -62,6 +64,15 @@ export default {
         quantity: this.quantity,
       };
       this.$store.commit("addToCart", item);
+
+      toast({
+        message: "The product was added to the cart",
+        type: "is-success",
+        dismissible: true,
+        pauseOnHover: true,
+        duration: 2000,
+        position: "bottom-right",
+      });
     },
   },
 };
