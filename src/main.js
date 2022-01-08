ccd73b17a30e4260262656b496e561278b641554
Earnest Scott <earnestscott@Earnests-MBP.lan>
2022-01-07 21:13:27 -0800

    create simple front page displaying latest products

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
-createApp(App).use(store).use(router).mount('#app')
+// allows us to specify just the endpoints when we use axios across our 
+// vue app (ie axios.get('/api/v1/products/') vs axios.get('localhost:port/api/v1/products'))
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
+// use() allows us to install a vue plugin
+createApp(App).use(store).use(router, axios).mount('#app')
