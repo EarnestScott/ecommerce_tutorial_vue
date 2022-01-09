03464013c06aa54b03a00d6e659d5080eb1f8c66
Earnest Scott <earnestscott@Earnests-MBP.lan>
2022-01-08 19:52:43 -0800

    implemented loading bar

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -41,11 +41,14 @@ export default {
     this.getProduct();
   },
   methods: {
-    getProduct() {
+    async getProduct() {
+      // set loading while awaiting response from api server
+      this.$store.commit("setIsLoading", true);
+
       const category_slug = this.$route.params.category_slug;
       const product_slug = this.$route.params.product_slug;
 
-      axios
+      await axios
         .get(`/api/v1/products/${category_slug}/${product_slug}/`)
         .then((response) => {
           this.product = response.data;
@@ -53,6 +56,7 @@ export default {
         .catch((error) => {
           console.log(error);
         });
+      this.$store.commit("setIsLoading", false);
     },
     addToCart() {
       // this.x refers to values returned by the data method above
