d0822583d2cd3786856a1821253b420446b9e4c7
Earnest Scott <earnestscott@Earnests-MBP.lan>
2022-01-08 20:59:37 -0800

    can now view category pages and converted product to vue component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,25 +12,11 @@
         <h2 class="is-size-2 has-text-centered">Latest products</h2>
       </div>
       <!-- loops through products in latestProducts -->
-      <div
-        class="column is-3"
+      <ProductBox
         v-for="product in latestProducts"
         v-bind:key="product.id"
-      >
-        <div class="box">
-          <figure class="image mb-4">
-            <img v-bind:src="product.get_thumbnail" />
-          </figure>
-          <h3 class="is-size-4">{{ product.name }}</h3>
-          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-          <router-link
-            v-bind:to="product.get_absolute_url"
-            class="button is-dark mt-4"
-          >
-            View details</router-link
-          >
-        </div>
-      </div>
+        v-bind:product="product"
+      />
     </div>
   </div>
 </template>
@@ -38,6 +24,9 @@
 <script>
 // @ is an alias to /src
 import axios from "axios";
+// @ is shortcut for root folder
+import ProductBox from "@/components/ProductBox";
+
 export default {
   name: "Home",
   data() {
@@ -45,7 +34,9 @@ export default {
       latestProducts: [],
     };
   },
-  components: {},
+  components: {
+    ProductBox,
+  },
   // vue lifecycle hook
   mounted() {
     this.getLatestProducts();
@@ -69,12 +60,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-/* removes spacing around image */
-.image {
-  margin-top: -1.25rem;
-  margin-left: -1.25rem;
-  margin-right: -1.25rem;
-}
-</style>
